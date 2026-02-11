949d4b90fb106a70edd8be45c973519e49fff524
Athrone Yael Rosal <athroneyael.rosal@cksc.college.edu.ph>
2026-02-11 08:08:47 +0800

    Practical Exam part 1

--- src/diary/DiaryList.tsx
+++ src/diary/DiaryList.tsx
@@ -1,62 +1,62 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import { blue } from "@mui/material/colors";
-import { moodList, sampleDiary, type DiaryEntryType } from "./Diary";
-import { useState } from "react";
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import Paper from "@mui/material/Paper"
+import { blue } from "@mui/material/colors"
+import { moodList, sampleDiary, type DiaryEntryType } from "./Diary"
+import { useState } from "react"
 
 function DiaryList() {
-  const diaryList = sampleDiary
 
-  return (
-    <>
-      {diaryList.map((entry, index) => (
-        <DiaryEntry entry={entry} key={index} />
-      ))}
-    </>
-  )
+    const diaryList = sampleDiary
+
+    return (
+        <>
+            {diaryList.map((entry, index) => (
+                <DiaryEntry entry={entry} key={index} />
+            ))}
+        </>
+    )
 }
 
 function DiaryEntry(prop: { entry: DiaryEntryType }) {
-  const { entry } = prop
-  const [expand, setExpand] = useState(false)
 
-  return (
-    <Paper
-      elevation={1}
-      sx={{
-        display: "flex",
-        p: 1,
-        backgroundColor: blue[100],
-      }}
-    >
-      <Typography sx={{ fontSize: "48px" }}>
-        {moodList[entry.mood].icon}
-      </Typography>
+    const { entry } = prop
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          pl: 1,
-        }}
-      >
-        <Typography sx={{ textAlign: "left" }}>
-          {entry.date.toUTCString()}
-        </Typography>
+    const [expand, setExpand] = useState(false)
 
-        <Typography onClick={() => setExpand(!expand)}>
-          {entry.title}
-        </Typography>
+    return (
+        <Paper elevation={1} sx={{
+            display: 'flex',
+            p: 1,
+            backgroundColor: blue[100],
+        }}>
 
-        {expand && (
-          <Typography>
-            {entry.content}
-          </Typography>
-        )}
-      </Box>
-    </Paper>
-  )
+            <Typography sx={{ fontSize: '48px' }}>
+                {moodList[entry.mood].icon}
+            </Typography>
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                flexGrow: 1,
+                pl: 1,
+            }}>
+                <Typography sx={{ textAlign: 'left' }}>
+                    {entry.date.toUTCString()}
+                </Typography>
+                <Typography onClick={() => setExpand(!expand)} >
+                    {entry.title}
+                </Typography>
+                {expand && (
+                    <Typography>
+                        {entry.content}
+                    </Typography>
+                )}
+            </Box>
+            <Typography sx={{ fontSize: '24px', color: '#cc9d02' }}>
+                {"★".repeat(entry.star)}
+            </Typography>
+        </Paper>
+    )
 }
 
 export default DiaryList
